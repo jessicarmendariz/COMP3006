840124a8dbff1114d7561fe1f969b5733e0a4434
Jessica Armendariz <87204977+jessicarmendariz@users.noreply.github.com>
2025-01-09 20:50:41 -0700

    Create notes.py
    1/9/25 class notes

---
+++ notes.py
@@ -0,0 +1,65 @@
+#January 07 - Week 1 - Day 1
+
+#There are homework guidelines for submitting each assignment
+#Functions and Variables = snake_case
+#Class Names = CamelCase
+
+
+
+
+
+#Janaury 09 - Week 1 - Day 2
+
+#Class names are written as:
+
+# class Point3D:
+#     pass
+
+# class Student():
+#     """
+#     Student Class: Creates little students...
+#     """
+#     pricePerCredit = 1500
+#     num_of_students = 0
+
+#     def __init__(self, first, last, credits):
+#         self.first_name = first
+#         self.last_name = last
+#         self.email = first + "." + last + "@du.edu"
+#         self.num_credits = credits
+#         self.tuitionCost = credits * self.pricePerCredit
+#         self.tuitionCost = credits * Student.pricePerCredit
+
+#         # Why use a class variable ?
+#         Student.num_of_students += 1
+    
+#     def fullname(self):
+#         return self.first_name + " " + self.last_name
+
+#     def tuition(self):
+#         self.tuitionCost = self.num_credits * Student.pricePerCredit
+
+# student1 = Student("Daniel", "Parada", 8)
+# print(student1.tuitionCost)
+# student1.pricePerCredit = 1400
+# student1.tuition()
+# print(student1.tuitionCost)
+
+# print(student1.tuitionCost)
+
+# # Let's check the name space for our class and instances :
+# print(Student.__dict__)
+# print(student1.__dict__)
+
+# # Make it more readable :
+# for attr in Student.__dict__:
+#     print(attr)
+
+# for attr in student1.__dict__.values():
+#     print(attr)
+
+
+
+
+
+#January 14 - Week 2 - Day 3 (Did Not Attend Class)
