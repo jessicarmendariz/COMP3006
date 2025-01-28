6f9274302bbe7cb69191deae7483ff1568bb5c9f
Jessica Armendariz <87204977+jessicarmendariz@users.noreply.github.com>
2025-01-28 14:10:27 -0700

    Update test_shelter.py

--- test_shelter.py
+++ test_shelter.py
@@ -10,3 +10,64 @@
 #• test_is_better – Checks that the is_better method is working as intended.
 
 #Every function must have a print statement that will print the message : “Executing test_name.” where test_name is the name of the current test being executed.
+
+import unittest
+from shelter import Tent, Tarp, Hammock  
+
+class TestShelter(unittest.TestCase):
+    #- This CLass is the Test Suite for Shelter and Inherits from Unit Test
+
+    #Runs Once Before All Tests and Instantiates Shelter and Assigns in to cls.data
+    @classmethod
+    def setUpClass(cls):
+        print("setUpClass")
+
+    #Runs Once After All Tests and Cleans Up
+    @classmethod
+    def tearDownClass(cls):
+        print("tearDown")
+
+    #Runs Before Each Test
+    def setUp(self):
+        print("Setting up test cases.")
+        self.tent1 = Tent(4, "nylon", 30, 100, True, 5, True, "4-season")
+        self.tent2 = Tent(2, "canvas", 45, 80, False, 7, False, "3-season")
+        self.tarp1 = Tarp("polyester", 10, 50)
+        self.tarp2 = Tarp("silnylon", 5, 30)
+        self.hammock1 = Hammock("parachute nylon", 15, 300, 2)
+        self.hammock2 = Hammock("cotton", 20, 250, 1)
+
+    #Runs After Each Test 
+    def tearDown(self):
+        print("Tearing down test cases.")
+        del self.tent1
+        del self.tent2
+        del self.tarp1
+        del self.tarp2
+        del self.hammock1
+        del self.hammock2
+
+    #Tests String Methods
+    def test_str(self):
+        print("Executing test_str.")
+        self.assertEqual(str(self.tent1), "Shelter(4, nylon, 30, 100, True, 5, True, 4-season)")
+        self.assertEqual(str(self.tarp1), "Shelter(polyester, 10, 50)")
+        self.assertEqual(str(self.hammock1), "Shelter(parachute nylon, 15, 300, 2)")
+
+    #Tests Less Than Methods
+    def test_lt(self):
+        print("Executing test_lt.")
+        self.assertTrue(self.tent1 < self.tent2)
+        self.assertTrue(self.tarp1 < self.tarp2)
+        self.assertTrue(self.hammock2 < self.hammock1)
+
+    #Tests Is Better Than Method
+    def test_is_better(self):
+        print("Executing test_is_better.")
+        self.assertTrue(self.tent1.is_better(self.tent2))
+        self.assertFalse(self.tarp2.is_better(self.tarp1))
+        self.assertTrue(self.hammock1.is_better(self.hammock2))
+
+#Runs Tests
+if __name__ == "__main__":
+    unittest.main()
